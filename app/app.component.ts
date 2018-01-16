75774f48eca2182a8b2864d9268cafb1797210d4
Gustavo Teles <gustavo.teles.ext@spms.min-saude.pt>
2018-01-16 14:55:15 +0000

    New commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import * as _ from "lodash";
 })
 export class AppComponent implements OnInit {
     chartResult: any[] = [];
-    label: boolean = false;
+    showLabelProperty: boolean = false;
 
     ngOnInit() {
         let data = [{"start":"2017-12-19T12:59:31.007Z","end":"2017-12-19T13:05:58.304Z","value":"346 count"},{"start":"2017-12-19T12:50:57.321Z","end":"2017-12-19T12:59:31.007Z","value":"260 count"},{"start":"2017-12-19T12:40:12.130Z","end":"2017-12-19T12:41:07.690Z","value":"55 count"},{"start":"2017-12-19T12:30:12.283Z","end":"2017-12-19T12:40:12.130Z","value":"60 count"},{"start":"2017-12-19T12:12:38.579Z","end":"2017-12-19T12:15:47.882Z","value":"27 count"},{"start":"2017-12-19T12:03:16.669Z","end":"2017-12-19T12:12:38.579Z","value":"152 count"},{"start":"2017-12-19T09:02:17.723Z","end":"2017-12-19T09:04:11.171Z","value":"42 count"},{"start":"2017-12-19T08:53:00.599Z","end":"2017-12-19T09:02:17.723Z","value":"432 count"},{"start":"2017-12-19T08:44:06.142Z","end":"2017-12-19T08:53:00.599Z","value":"211 count"},{"start":"2017-12-19T08:31:34.791Z","end":"2017-12-19T08:37:27.763Z","value":"183 count"},{"start":"2017-12-19T08:24:56.284Z","end":"2017-12-19T08:31:34.791Z","value":"23 count"},{"start":"2017-12-19T08:16:45.160Z","end":"2017-12-19T08:24:56.284Z","value":"239 count"},{"start":"2017-12-19T07:56:05.983Z","end":"2017-12-19T08:02:30.153Z","value":"743 count"},{"start":"2017-12-19T07:46:08.378Z","end":"2017-12-19T07:56:05.983Z","value":"455 count"}];
@@ -34,18 +34,17 @@ export class AppComponent implements OnInit {
 
         console.log('Chart : ' + JSON.stringify(this.chartResult));
 
-
     }
 
-    onSeriesSelected(args) {
-        console.log("onSeriesSelected");
-        this.label = true;
+    onPointSelected(args) {
+        let index = args.pointIndex;
+        console.log('index selected: ' + index);
+        this.showLabelProperty = true;
+    }
 
-        for (let key in args) {
-            if (args.hasOwnProperty(key)) {
-                let element = args[key];
-                console.log(key + ": " + element);
-            }
-        }
+    onPointDeselected(args){
+        let index = args.pointIndex;
+        console.log('index deselected: ' + index);
+        this.showLabelProperty = false;
     }
 }
